df91b3ecc9915f3ea02cfd643fc0357d500bf7ea
Gabriela Arroyo <gabriela.hispanicas@gmail.com>
2025-04-08 01:42:14 -0600

    Add new changes to my project

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+let profileName = document.querySelector(".header__profile_name");
+let profileDescription = document.querySelector(".header__description");
+let editButton = document.querySelector(".header__edit-button");
+let popup = document.querySelector(".popup");
+let popupCloseButton = document.querySelector(".popup__container_close-icon");
+let inputName = document.querySelector("#inputName");
+let inputDescription = document.querySelector("#inputDescription");
+let formProfileInformation = document.querySelector(".form");
+
+function handleOpenPopup() {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function handleChangeInformation(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closePopup();
+}
+
+editButton.addEventListener("click", handleOpenPopup);
+formProfileInformation.addEventListener("submit", handleChangeInformation);
+popupCloseButton.addEventListener("click", function () {
+  closePopup();
+});
