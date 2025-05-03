342d4b69a6d68b30084e4b76efe1a445914b8bf8
Gabriela Arroyo <gabriela.hispanicas@gmail.com>
2025-05-03 00:14:43 -0600

    Add new changes to my project

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,9 @@ const popupCloseButtons = document.querySelectorAll(".popup__close-icon");
 const inputName = document.querySelector("#inputName");
 const inputDescription = document.querySelector("#inputDescription");
 const formProfileInformation = document.querySelector(".form");
+/*Nuevo código*/
+const galleryTemplate = document.querySelector("#gallery-template");
+const galleryCardsContainer = document.querySelector(".gallery");
 
 function handleOpenPopup() {
   inputName.value = profileName.textContent;
@@ -23,6 +26,22 @@ function handleChangeInformation(evt) {
   profileDescription.textContent = inputDescription.value;
   closePopup();
 }
+
+/*Nuevas funciones*/
+function createGallery(name, link) {
+  const galleryCard = galleryTemplate
+    .cloneNode(true)
+    .content.querySelector(".gallery__photo");
+
+  const galleryImage = galleryCard.querySelector(".gallery__about-places");
+  const galleryText = galleryCard.querySelector(".gallery__name-place");
+
+  galleryImage.src = link;
+  galleryText.textContent = name;
+  galleryCardsContainer.append(galleryCard);
+}
+
+createGallery();
 
 editButton.addEventListener("click", handleOpenPopup);
 formProfileInformation.addEventListener("submit", handleChangeInformation);
