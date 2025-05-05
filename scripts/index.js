99fe1c9590eddfa268c3d53e49e0a6ba07f1e23a
Gabriela Arroyo <gabriela.hispanicas@gmail.com>
2025-05-05 03:18:52 -0600

    Add new changes to my pŕoject

--- scripts/index.js
+++ scripts/index.js
@@ -15,6 +15,39 @@ const galleryAddButton = document.querySelectorAll(".header__create-button");
 const inputCardTitle = document.querySelector("#input-card-title");
 const inputCardImage = document.querySelector("#input-card-image");
 const formCreateCard = document.querySelector("#form-create-card");
+/*Aquí moví el form antes de los input y a ellos los llamo desde el form*/
+
+const initialCards = [
+  {
+    name: "Porcupine Mountains",
+    link: "https://images.unsplash.com/photo-1601227956428-6536f5e30920?q=80&w=1378&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    name: "Cataratas Redrock",
+    link: "https://images.unsplash.com/photo-1736633920290-199df87d7836?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    name: "Monte Rainier",
+    link: "https://images.unsplash.com/photo-1573331592635-f658507c1e8c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    name: "Sequoia National Park",
+    link: "https://images.unsplash.com/photo-1516213240617-f50eaeda237e?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    name: "Gran Fuente Prismática",
+    link: "https://images.unsplash.com/photo-1612390388573-45904a65dd0f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+
+  {
+    name: "Valle de Yosemite",
+    link: "https://images.unsplash.com/photo-1488441770602-aed21fc49bd5?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
 
 function handleOpenPopup() {
   inputName.value = profileName.textContent;
@@ -39,6 +72,12 @@ function handleChangeInformation(evt) {
 }
 
 /*Nuevas funciones*/
+
+initialCards.forEach((item) => {
+  const card = createGallery(item.name, item.link);
+  galleryCardsContainer.append(card);
+});
+
 function createGallery(name, link) {
   const galleryCard = galleryTemplate
     .cloneNode(true)
@@ -53,8 +92,6 @@ function createGallery(name, link) {
 
   return galleryCard;
 }
-
-createGallery();
 
 /*código del sprint 7 */
 editButton.addEventListener("click", handleOpenPopup);
