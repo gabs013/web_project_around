11c84c132594db4ad5a69f05d60b7351c609f21d
Gabriela Arroyo <gabriela.hispanicas@gmail.com>
2025-05-26 00:00:57 -0600

    Add form validation functions

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,22 +1,32 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (
+  formElement,
+  inputElement,
+  errorMessage,
+  configForm
+) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("popup__input_type_error");
+  inputElement.classList.add(configForm.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup__input-error_active");
+  errorElement.classList.add(configForm.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, configForm) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("popup__input_type_error");
-  errorElement.classList.remove("popup__input-error_active");
+  inputElement.classList.remove(configForm.inputErrorClass);
+  errorElement.classList.remove(configForm.errorClass);
   errorElement.textContent = "";
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, configForm) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      configForm
+    );
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, configForm);
   }
 };
 
@@ -26,45 +36,58 @@ const hasInvalidInput = (inputList) => {
   });
 };
 
-const toggleButtonState = (inputList, buttonElements) => {
+const toggleButtonState = (inputList, buttonElements, configForm) => {
   const invalid = hasInvalidInput(inputList);
   buttonElements.forEach((buttonElement) => {
     if (invalid) {
-      buttonElement.classList.add("button_inactive");
+      buttonElement.classList.add(configForm.inactiveButtonClass);
       buttonElement.disabled = true;
     } else {
-      buttonElement.classList.remove("button_inactive");
+      buttonElement.classList.remove(configForm.inactiveButtonClass);
       buttonElement.disabled = false;
     }
   });
 };
 
-const setEventListeners = (formElement) => {
+const setEventListeners = (formElement, configForm) => {
   /*Me quedé en esta línea de código. Se supone está bien hecha.*/
-  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+  const inputList = Array.from(
+    formElement.querySelectorAll(configForm.inputSelector)
+  );
   const submitButtons = Array.from(
-    formElement.querySelectorAll(".popup__submit")
+    formElement.querySelectorAll(configForm.submitButtonSelector)
   );
   /*Prueba: Validar estado inicial del botón.*/
-  toggleButtonState(inputList, submitButtons);
+  toggleButtonState(inputList, submitButtons, configForm);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, submitButtons);
+      checkInputValidity(formElement, inputElement, configForm);
+      toggleButtonState(inputList, submitButtons, configForm);
     });
   });
 };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".form"));
+const enableValidation = (configForm) => {
+  const formList = Array.from(
+    document.querySelectorAll(configForm.formSelector)
+  );
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
     });
 
-    setEventListeners(formElement);
+    setEventListeners(formElement, configForm);
   });
 };
 
-enableValidation();
+const configForm = {
+  formSelector: ".form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "button_inactive",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+};
+
+enableValidation(configForm);
