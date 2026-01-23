80b17341276d1d9974fa9a1ca119063373f345e2
Gabriela Arroyo <gabriela.hispanicas@gmail.com>
2026-01-23 10:56:34 -0600

    Add clean comments to my Api

--- components/Api.js
+++ components/Api.js
@@ -1,12 +1,10 @@
-//Código prueba
-// Api.js
 export default class Api {
   constructor({ baseUrl, headers }) {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
 
-  // Método privado para verificar la respuesta (¡Ayuda mucho!)
+  //Método privado para verificar la respuesta
   _checkResponse(res) {
     if (res.ok) {
       return res.json();
@@ -14,21 +12,21 @@ export default class Api {
     return Promise.reject(`Error: ${res.status}`);
   }
 
-  // 1. Obtener información del usuario
+  //Obtiene información del usuario
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
     }).then(this._checkResponse);
   }
 
-  // 2. Obtener el array inicial de tarjetas
+  //Obtiene el array inicial de tarjetas
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
     }).then(this._checkResponse);
   }
 
-  // 3. Editar el perfil
+  //Edita el perfil
   editUserInfo({ name, about }) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
@@ -37,7 +35,7 @@ export default class Api {
     }).then(this._checkResponse);
   }
 
-  // 4. Agregar una nueva tarjeta
+  //Agrega una nueva tarjeta
   addNewCard({ name, link }) {
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
@@ -46,7 +44,7 @@ export default class Api {
     }).then(this._checkResponse);
   }
 
-  // 7. Eliminar una tarjeta
+  //Elimina una tarjeta
   deleteCard(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}`, {
       method: "DELETE",
@@ -54,7 +52,7 @@ export default class Api {
     }).then(this._checkResponse);
   }
 
-  // 8. Dar "like" a una tarjeta
+  //Da "like" a una tarjeta
   addLike(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: "PUT",
@@ -62,7 +60,7 @@ export default class Api {
     }).then(this._checkResponse);
   }
 
-  // 8. Quitar "like" a una tarjeta
+  //Quita "like" a una tarjeta
   removeLike(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: "DELETE",
@@ -70,7 +68,7 @@ export default class Api {
     }).then(this._checkResponse);
   }
 
-  // 9. Actualizar la foto de perfil
+  //Actualiza la foto de perfil
   updateUserAvatar({ avatar }) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
@@ -80,11 +78,11 @@ export default class Api {
   }
 }
 
-// INSTANCIA DE LA API (¡USO DE MI TOKEN!)
+// Instancio de la API (¡Uso de mi Token!)
 export const api = new Api({
   baseUrl: "https://around-api.es.tripleten-services.com/v1",
   headers: {
-    authorization: "0d8cff7c-aff7-40ae-b2cb-b0a409b0e764", // <-- ¡Mi Token!
+    authorization: "0d8cff7c-aff7-40ae-b2cb-b0a409b0e764", //Mi Token
     "Content-Type": "application/json",
   },
 });
